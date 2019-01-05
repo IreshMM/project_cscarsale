c013765362c20a169e2d3f8a494dcdd1fe81bfc0
Hiran <Hirantharinda@gmail.com>
2019-01-06 04:32:54 +0530

    Report Update

--- resources/views/Finance/chart.blade.php
+++ resources/views/Finance/chart.blade.php
@@ -7,7 +7,7 @@
 @section('content')
   @include('Reports/tablesProfit')
   <br><br><br>
-  <!-- @include('Reports/charttype') -->
   @include('Reports/chartpie')
   @include('Reports/tablesSales')
+
 @endsection
